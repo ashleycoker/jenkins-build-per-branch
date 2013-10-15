32fba1779ef7ec091e9cf8a017a808014bbb37b2
ashleycoker <ashley.coker@prowebsoftware.net>
2013-10-15 06:41:15 +0100

    Update JenkinsApi.groovy

--- src/main/groovy/com/entagen/jenkins/JenkinsApi.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsApi.groovy
@@ -59,8 +59,7 @@ class JenkinsApi {
         post('createItem', missingJobConfig, [name: missingJob.jobName, mode: 'copy', from: templateJob.jobName], ContentType.XML)
 
         post('job/' + missingJob.jobName + "/config.xml", missingJobConfig, [:], ContentType.XML)
-        sleep 10000
-        post('job/' + missingJob.jobName + "/config.xml", missingJobConfig, [:], ContentType.XML)
+        post('job/' + missingJob.jobName + "/description", 'missingJobConfig', [:], ContentType.XML)
     }
 
     void startJob(ConcreteJob job) {
